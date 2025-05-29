01180bcf36bc6da0e95738d017400fc83f14db5b
rumi-tetsuya-n <rumi1008t@gmail.com>
2025-05-29 14:34:17 +0900

    007-3

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,18 +1,14 @@
-let num = Math.floor(Math.random() * 100);
+const num = 15;
 
-if(num % 3 === 0 && num % 5 ===0 ) {
-  console.log('3と5の倍数です');
+if ( num%3 === 0 && num%5 === 0){
+    console.log("3と5の倍数です");
 }
-
-
-if(num % 3 === 0 && num % 5 !== 0 ) {
-  console.log('3の倍数です');
+else if ( num%5 === 0 ){
+    console.log("5の倍数です");
 }
-
-else if (num % 3 !== 0 && num %5 ===0 ) {
-  console.log('5の倍数です');
-} 
-
-else {
-  console.log(num);
+else if ( num%3 === 0 ){
+    console.log("3の倍数です");
+}
+else{
+    console.log(num);
 }
